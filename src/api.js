a6f7764bbcdfd7d7c0956241748b76d8d17d6751
paviethra <62457057+paviethra@users.noreply.github.com>
2022-09-21 11:58:29 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b605c7d449mshb51272453d2f8f2p187e1fjsn6c6fce4d5ad7",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "68d0222c62a187e2c2f79ce16e56cfa7"; // enter your key from openweather API
